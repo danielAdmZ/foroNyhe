52e9629a09e72ac779a1ff9576d5984b4709bc81
DanielAdmZ <danielvadcom@hotmail.com>
2024-07-08 18:01:37 -0500

    Inicio de repositorio

---
+++ src/main/java/com/nyhestudios/foroNyhe/foroNyhe/ForoNyheApplication.java
@@ -0,0 +1,13 @@
+package com.nyhestudios.foroNyhe.foroNyhe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoNyheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoNyheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nyhestudios/foroNyhe/foroNyhe/ForoNyheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyhestudios.foroNyhe.foroNyhe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoNyheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
